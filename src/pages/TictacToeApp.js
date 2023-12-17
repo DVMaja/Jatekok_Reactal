b2fbfff9f3abdb68b9415d7d7e6c487bd9315c5c
DVMaja <majadreilinger@gmail.com>
2023-12-17 21:08:11 +0100

    a Tic Tac Toe kattintható, és beírja az X v O, mappaszerk megvan

--- src/pages/TictacToeApp.js
+++ src/pages/TictacToeApp.js
@@ -1,5 +1,31 @@
+import Jatekter from '../komponensek/tictactoe/Jatekter';
+import { useState } from 'react';
+import Modell from '../model/tictactoe/Model.js';
+const tmodel = new Modell();
+
 export default function TictacToeApp() {
+
+    const [list, setList] = useState(tmodel.getList()); //Ez volt["X", "O", "", "", "", "", "", "", "",]
+
+    function katt(index) {
+        //egyenlőre írja be a X et
+        //és írja be a state  et
+        /**Miért fontosak, ha az értke megváltozik, akkor fog az oldalon csak az a dolog változik meg */
+        console.log(index);
+        tmodel.setAllapot(index);
+        setList(tmodel.getList()) // [...lista] tényleges új másolat
+
+
+    }
+
     return (
-        <div className="tictactoe">Tic Tac Toe játék</div>
+        <div className="tictactoe">
+            <header>
+                <h1>Tic Tac Toe játék</h1>
+            </header>
+            <article>
+                <Jatekter lista={list} katt={katt} />
+            </article>
+        </div>
     )
 }
